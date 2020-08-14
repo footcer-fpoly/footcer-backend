d3a4552228d221a0bec5833b68bb898e10854ccc
mrlongg71 <mrlongg71@gmail.com>
2020-08-14 17:13:10 +0700

    update key production

--- security/pro/key.go
+++ security/pro/key.go
@@ -2,10 +2,10 @@ package pro
 
 var (
 	JWT_KEY        = "footcer6969696969969696996969699696996"
-	HOST           = "host.docker.internal" //35.184.207.203
+	HOST           = "35.184.70.223" //35.226.61.216
 	PORT           = 5432
-	USERNAME       = "postgres"
-	PASSWORD       = "123456"
+	USERNAME       = "footcerdb"
+	PASSWORD       = "PMa45PqnMHiHCn40"
 	DB_NAME        = "footcerdb"
 	GOOGLE_MAP_KEY = "AIzaSyAdzuKh5c4g1CyHY8W_yS5rTmAqKI6yf4Q"
 )
