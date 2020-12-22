205530670930cc6d25c9706d7da57ed883f5e5fd
Nguyễn Hoàng Long <41661101+MrLongg71@users.noreply.github.com>
2020-12-22 21:29:39 +0700

    Update key.go

--- security/pro/key.go
+++ security/pro/key.go
@@ -7,6 +7,6 @@ var (
 	USERNAME        = "footcerdb"
 	PASSWORD        = "PMa45PqnMHiHCn40"
 	DB_NAME         = "footcerdb"
-	GOOGLE_MAP_KEY  = "AIzaSyCo6vCjX565rbXZ2OSzz0T3sIEG5hBSw0A"
+	GOOGLE_MAP_KEY  = "AIzaSyAmh-Tqfy35GzzQlGED6HLigQtXN4dMi7Q"
 	FIREBASE_SERVER = "AAAAljhapk4:APA91bGSG3ccP7le_pEbw_0kXnjBRNqYKPXRabn2rsybPk6eL4C0jrRhUpCnpe5gidPQcHdQKNgEOvzPuLJMS7zQWz41NJBrFVKNwz-KJTS76u-Eow4wa4kUc3DOhLjvYLrqV2kpZU3R"
 )
